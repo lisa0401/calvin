fe9bffad49263d4fe59b7e2b461cba1f438c9052
Lisa Ozaki <lisaozaki0402@keio.jp>
2025-08-04 11:36:13 +0900

    src_calvin_occ_working

--- calvin_occ.sh
+++ calvin_occ.sh
@@ -6,7 +6,7 @@ if [ "$1" == "m" ]; then
 elif [ "$1" == "t" ]; then
     ARGUMENT="t"
 else
-    ARGUMENT="m"  # デフォルト値を"m"とする
+    ARGUMENT="t"  # デフォルト値を"m"とする
 fi
 
 # 必要であれば LD_LIBRARY_PATH を有効化
